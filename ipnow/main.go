be8815cc1a087980880e2bcb2e1a22744e92d5c7
Pekka Heino <pxi@usr.fi>
2016-02-10 01:42:12 +0200

    ipnow: initial commit.

---
+++ ipnow/main.go
@@ -0,0 +1,57 @@
+// Ipnow prints the current IP address.
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("ipnow: ")
+
+	a, err := ipAddr()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(a)
+}
+
+func ipAddr() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("no ip")
+}
